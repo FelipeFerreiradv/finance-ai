d8164d6f6b43079109e02470285db7fe23de511b
Felipe Ferreira <fefelbf@gmail.com>
2024-12-04 13:39:33 -0300

    feat: add a responsive design style

--- app/components/ui/navbar.tsx
+++ app/components/ui/navbar.tsx
@@ -4,11 +4,12 @@ import Image from 'next/image';
 import { UserButton } from '@clerk/nextjs';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import NavbarMobile from './navbar-mobile';
 
 const Navbar = () => {
   const pathname = usePathname();
   return (
-    <header className="flex items-center justify-between w-full h-[60px] border-solid border-2">
+    <header className="flex items-center justify-between w-screen h-[60px] border-solid border-2 overflow-hidden">
       <nav className="flex items-center gap-[4rem] ml-[2rem]">
         <div>
           <Image
@@ -19,39 +20,42 @@ const Navbar = () => {
           />
         </div>
 
-        <Link
-          href="/dashboard/?month=9"
-          className={
-            pathname === '/dashboard'
-              ? 'text-[#55B02E] font-bold'
-              : 'text-white  hover:text-[#55B02E]'
-          }
-        >
-          Dashboard
-        </Link>
-        <Link
-          href="/transactions"
-          className={
-            pathname === '/transactions'
-              ? 'text-[#55B02E] font-bold'
-              : 'text-white  hover:text-[#55B02E]'
-          }
-        >
-          Transações
-        </Link>
-        <Link
-          href="/subscription"
-          className={
-            pathname === '/subscription'
-              ? 'text-[#55B02E] font-bold'
-              : 'text-white  hover:text-[#55B02E]'
-          }
-        >
-          Assinatura
-        </Link>
+        <div className="flex items-center gap-[4rem] max-md:hidden">
+          <Link
+            href="/dashboard/?month=9"
+            className={
+              pathname === '/dashboard'
+                ? 'text-[#55B02E] font-bold'
+                : 'text-white  hover:text-[#55B02E]'
+            }
+          >
+            Dashboard
+          </Link>
+          <Link
+            href="/transactions"
+            className={
+              pathname === '/transactions'
+                ? 'text-[#55B02E] font-bold'
+                : 'text-white  hover:text-[#55B02E]'
+            }
+          >
+            Transações
+          </Link>
+          <Link
+            href="/subscription"
+            className={
+              pathname === '/subscription'
+                ? 'text-[#55B02E] font-bold'
+                : 'text-white  hover:text-[#55B02E]'
+            }
+          >
+            Assinatura
+          </Link>
+        </div>
       </nav>
-      <div className="mr-[2rem]">
-        <UserButton showName />
+      <div className="flex items-center gap-4 mr-[2rem]">
+        <NavbarMobile />
+        <UserButton />
       </div>
     </header>
   );
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,9 +12,9 @@ const LoginPage = async () => {
   }
 
   return (
-    <div className="flex w-full h-full">
+    <div className="flex w-full h-full max-xl:flex-col-reverse">
       {/* esquerda */}
-      <div className="flex flex-col justify-center w-1/2 h-full p-32">
+      <div className="flex flex-col justify-center w-1/2 h-full p-32 max-md:p-10">
         <div className="flex flex-col gap-7 min-w-[550px]">
           <Image
             src="/logo-svg.svg"
@@ -22,21 +22,26 @@ const LoginPage = async () => {
             width={200}
             height={50}
           />
-          <h1 className="font-bold text-5xl mt-[1rem]">Bem-vindo</h1>
-          <p className="text-2xl text-muted-foreground">
+          <h1 className="font-bold text-5xl mt-[1rem] max-sm:text-4xl">
+            Bem-vindo
+          </h1>
+          <p className="text-2xl text-muted-foreground max-md:max-w-[500px] max-sm:max-w-[300px] max-sm:text-[16px]">
             A Finance AI é uma plataforma de gestão financeira que utiliza IA
             para monitorar suas movimentações, e oferecer insights
             personalizados, facilitando o controle do seu orçamento.
           </p>
           <SignInButton>
-            <Button variant="outline" className="max-w-[80%] h-[50px] radius">
+            <Button
+              variant="outline"
+              className="max-w-[80%] h-[50px] radius max-sm:max-w-[50%]"
+            >
               <FcGoogle className="mr-2" /> Entrar com Google
             </Button>
           </SignInButton>
         </div>
       </div>
       {/* direita */}
-      <div className="relative w-1/2 h-full">
+      <div className="relative w-1/2 h-full max-xl:w-screen">
         <Image
           src="/login-two.png"
           alt="image of layout finnance"
